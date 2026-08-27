18942aa215c05452916743fd2eafa2b37e23eec9
Mani-syntax <2300040093ecemdie@gmail.com>
2026-08-27 12:53:58 +0530

    fix(map): replace CartoDB tile layer with Esri World Street Map to fix API key watermarks on production, and ignore uploads/ in gitignore

--- src/components/PropertyDetailsPage.tsx
+++ src/components/PropertyDetailsPage.tsx
@@ -26,8 +26,8 @@ const PropertyLocationMap: React.FC<{ latitude: number; longitude: number; title
       zoomControl: true
     });
     
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ',
       maxZoom: 20,
     }).addTo(map);
     
--- src/components/ui/LiveLocationMap.tsx
+++ src/components/ui/LiveLocationMap.tsx
@@ -76,8 +76,8 @@ export const LiveLocationMap: React.FC<LiveLocationMapProps> = ({
       keyboard: false,
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ',
       maxZoom: 20,
     }).addTo(map);
 
